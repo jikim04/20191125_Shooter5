af0bce054eb79d453d59e3474d3d77c38c0c540d
jikim04 <55118179+jikim04@users.noreply.github.com>
2019-11-25 21:06:10 +0900

    add

---
+++ Assets/02.Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    // Use this for initialization
+    public enum State {
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+    public State state = State.PATROL;
+    private Transform playerTr;
+    private Transform enemyTr;
+    public float attackDist = 5.0f;
+    public float traceDist = 10.0f;
+    public bool isDie = false;
+    private WaitForSeconds ws;
+
+    void Awake()
+    {
+        var player = GameObject.FindGameObjectsWithTag("PLAYER");
+
+        if (player != null)
+            playerTr = player.GetComponent<Transform>();
+
+        enemyTr = GetComponent<Transform>();
+
+        ws = new WaitForSeconds(0.3f);
+
+
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(CheckState());
+    }
+
+    IEnumerator CheckState()
+    {
+        while (!isDie)
+        { if (state == State.DIE) yield break;
+            float dist = Vector3.Distance(playerTr.position, enemyTr.position);
+
+            if (dist <= attackDist)
+            { state = State.ATTACK; }
+
+            else if (dist <= traceDist)
+            { state = State.TRACE;  }
+
+            else
+            { state = State.PATROL; }
+
+            yield return ws;
+        }
+    }
+
+}
---
+++ Assets/02.Scripts/Enemy/MoveAgent.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveAgent : MonoBehaviour {
+    public List<Transform> wayPoints;
+    public int nexidx;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        var group = GameObject.Find("WayPointGroup");
+        if (group != null)
+        {
+            group.GetComponentsInChildren<Transform>(wayPoints);
+            wayPoints.RemoveAt(0);
+        }
+        MoveWayPoint();
+	}
+
+    void MoveWayPoint()
+    {
+        if (agent.isPathStale) return;
+
+        agent.destination = wayPoints[nexidx].position;
+        agent.isStopped = false;
+            }
+
+	// Update is called once per frame
+	void Update () {
+        if (agent.velocity.sqrMagnitude >= 0.2f * 0.2f
+            && agent.remainingDistance <= 0.5f)
+        {
+            nexidx = ++nexidx % wayPoints.Count;
+            MoveWayPoint();
+        }
+	}
+}
